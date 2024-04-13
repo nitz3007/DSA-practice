3008a54f5b14679e5457714c588756dff836ddbc
nitz3007 <niharikat80@gmail.com>
2024-04-13 13:34:19 +0530

    A2Z DSA Sheet: Lec2 Pattern-1

---
+++ Patterns/Pattern1.cpp
@@ -0,0 +1,23 @@
+// Question: Print the following pattern for a given input number n:
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPattern1(int n) {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+int main() {
+    int n;
+    cin>>n;
+    printPattern1(n);
+}
