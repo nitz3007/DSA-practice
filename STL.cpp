2513d4b1bda8f6967d2d61606f32107ec132b7bc
nitz3007 <niharikat80@gmail.com>
2024-05-02 11:13:26 +0530

    queue

--- STL.cpp
+++ STL.cpp
@@ -112,6 +112,22 @@ void explainStack() {
     cout<<st.top()<<endl;
 }
 
+//queue
+void explainQueue() {
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.emplace(4);
+    
+    q.back() +=5;
+    
+    cout<<q.back()<<endl;
+    
+    cout<<q.front()<<endl;
+    q.pop();
+    cout<<q.front()<<endl;
+}
+
 int main() {
     explainPairs();
     return 0;
