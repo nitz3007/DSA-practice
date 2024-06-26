5157744631533665f2a630d1d4eb095d8b217e4b
nitz30 <niharikat80@gmail.com>
2024-06-27 00:44:00 +0530

    print all divisors

--- BasicMaths/PrintAllDivisors.cpp
+++ BasicMaths/PrintAllDivisors.cpp
@@ -3,26 +3,26 @@ using namespace std;
 
 void printDivisors(int N) {
     vector<int> v;
-    //time complexity: O(sqrt(N))
     for(int i=1; i*i<=N; i++) {
-        if(N%i == 0){
+        if(N%i == 0) {
             v.push_back(i);
+            
             if(N/i != i) {
                 v.push_back(N/i);
             }
         }
     }
-    //time complexity: O(x * log(x)), where x = no. of divisors
-    sort(v.begin(), v.end());
-
-    //time complexity: O(x)
-    for(auto it: v){
+    sort(v.begin(),v.end());
+        
+    for(auto it:v) {
         cout<<it<<" ";
     }
 }
 
 int main() {
-    
-    printDivisors(36);
-    return 0;
+    int N;
+    cin>>N;
+    printDivisors(N);
+
+	return 0;
 }
