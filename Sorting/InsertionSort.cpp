a4c4ffd12bf8b2e7765e3b33a03f9fa6f5a7104a
nitz30 <niharikat80@gmail.com>
2024-08-01 00:40:53 +0530

    Bubble & Insertion sort

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArray(int arr[], int n) {
+    for(int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    }
+}
+
+
+void insertionSort(int arr[], int n) {
+    for(int i=0; i<n; i++) {
+        int j=i;
+        while(j>0 && arr[j]<arr[j-1]) {
+            swap(arr[j-1], arr[j]);
+            j--;
+        }
+    }
+    
+}
+
+int main() {
+    // Write C++ code here
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+    insertionSort(arr,n);
+    printArray(arr,n);
+    
+    return 0;
+}
